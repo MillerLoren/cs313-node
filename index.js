e081156d587dbd9bc88b7107a13d32829d70ff4b
Loren Miller <lorenmillerp@gmail.com>
2018-03-10 18:13:22 -0700

    Completed Prove09

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template files
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
@@ -14,11 +13,21 @@ app.get('/', function(request, response) {
   response.render('pages/index')
 });
 
-app.get('/cool', function(request, response) {
-  response.send(cool());
+app.get('/assignments',function(req,res){
+  res.render('pages/assignments')
+});
+
+app.get('/postalCalc', function(req,res){
+  res.render('pages/postalCalc')
+});
+
+app.post('/getRate',function(req,res){
+  var type = req.body.mailType;
+  var weight = req.body.weight;
+
+  res.render('pages/getRate', {type: type, weight: weight});
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
---
+++ public/scripts/script.js
@@ -0,0 +1,137 @@
+$(window).on('resize', toggle);
+$(window).on('load', toggle);
+$(window).on('load', toggleWeight);
+$(".edit").on('click',function() {
+	editContact(this.closest( "tr" ));
+});
+$(".add").on('click',function() {
+	addContact();
+});
+$(".trash").on('click',function() {
+	deleteContact(this.closest( "tr" ));
+});
+$('#calcForm').change(function(){
+	toggleWeight();
+})
+function toggleWeight(){
+	var select = document.getElementById("calcFormSelect").value;
+	if(select == 'post'){
+		document.getElementById("weightRow").style.display = "none";
+	}else{
+		document.getElementById("weightRow").style.display = "table-row";
+	}
+}
+var arr = [];
+var cartCount = 0;
+function editContact(tr){
+	var max = tr.childNodes.length;
+	for(var i = 0; i < max; i++){
+		var text = tr.childNodes[i].innerText || tr.childNodes[i].textContent;
+		var td = $("<td></td>");
+		switch(i){
+			case 0:
+				var input = text;
+			break;
+			case 1:
+				var input = "<input name='num' type='hidden' value='" + tr.childNodes[1].childNodes[0].value + "'/><input name='name' type='text' value='" + text + "'/>";
+			break;
+			case 2:
+				var input = "<input name='email' type='text' value='" + text + "'/>";
+			break;
+			case 3:
+				var input = "<input name='phone' type='text' value='" + text + "'/>";
+			break;
+			case 4:
+				var input = "<input name='company' type='text' value='" + text + "'/>";
+			break;
+			case 5:
+				var input = "<input name='title' type='text' value='" + text + "'/>";
+			break;
+			case 6:
+				var input = "<input type='submit' class='check' name='update' />/<img class='cancel' onclick='cancelEdit()' src='../../Images/cancel.png' />"
+			break;
+		}
+		tr.childNodes[i].innerHTML = input;
+		$(".edit").remove();
+		$(".trash").remove();
+		$(".add").remove();
+	}
+}
+function cancelEdit(){
+	location.reload();
+}
+function addContact(){
+	var table = $("#contactsTable tr:last");
+	var num = $("#contactsTable tr").length;
+	var tr = "<tr><td>" + num + "</td><td><input name='name' type='text' value=''></td><td><input name='email' type='text' value=''></td><td><input name='phone' type='text' value=''></td><td><input name='company' type='text' value=''></td><td><input name='title' type='text' value=''></td><td><input type='submit' class='check' name='add' src='../../Images/check.png'>/<img class='cancel' onclick='cancelEdit()' src='../../Images/cancel.png'></td></tr>";
+	table.after(tr);
+	$(".edit").remove();
+	$(".trash").remove();
+	$(".add").remove();
+}
+function deleteContact(tr){
+	tr.childNodes[0].innerHTML = "<input name='num' type='hidden' value='" + tr.childNodes[1].childNodes[0].value + "'/>" + tr.childNodes[0].innerHTML;
+
+}
+function toggle(){
+	var win = $(this); //this = window
+	var ul = document.getElementById("menu");
+	var btn = document.getElementById("dropBtn");
+	if (win.width() <= 700) {
+	  $(ul).removeClass('show');
+	  $(ul).addClass('hide');
+	  $(btn).removeClass('hide');
+	  $(btn).addClass('show');
+	}
+	if (win.width() >= 701) {
+	  $(ul).removeClass('hide');
+	  $(ul).addClass('show');
+	  $(btn).removeClass('show');
+	  $(btn).addClass('hide');
+	}
+}
+function menu(){
+	var ul = document.getElementById("menu");
+	if($(ul).hasClass('hide') == true){
+		$(ul).removeClass('hide');
+		$(ul).addClass('show');
+	}else{
+		$(ul).removeClass('show');
+		$(ul).addClass('hide');
+	}
+}
+function addToCart(item){
+	console.log("Item Number", item);
+	var div = document.getElementById(item);
+	var hidden = document.getElementById("viewCart");
+	var hidden1 = document.getElementById("viewCart1");
+	var hidden2 = document.getElementById("viewCart2");
+	var hidden3 = document.getElementById("viewCart3");
+	var quantity = document.getElementsByClassName("quantity")[item - 1].value;
+	console.log("Quantity", quantity);
+	if(quantity > 0 && quantity != "" && quantity != 0){
+		console.log("Switch", item);
+		switch(item){
+			case 1:
+				var tempArr = {Game:"Ark: Survival Evolved", Price:"$39.99", Quantity:quantity};
+				arr[0] = tempArr;
+				hidden1.value = "";
+				hidden1.value = JSON.stringify(arr);
+				break;
+			case 2:
+				var tempArr = {Game:"Overwatch", Price:"$59.99", Quantity:quantity};
+				arr[1] = tempArr;
+				hidden2.value = "";
+				hidden2.value = JSON.stringify(arr);
+				break;
+			case 3:
+				var tempArr = {Game:"RocketLeague", Price:"$29.99", Quantity:quantity};
+				arr[2] = tempArr;
+				hidden3.value = "";
+				hidden3.value = JSON.stringify(arr);
+				break;
+		}
+	}
+	hidden.value = "";
+	hidden.value = JSON.stringify(arr);
+}

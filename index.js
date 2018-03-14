8df2391761ed68a87c76ab0ae43a06869adcb69c
Loren Miller <lorenmillerp@gmail.com>
2018-03-14 12:08:45 -0600

    Fixed database connection

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var pgp = require('pg-promise')();
+
+var db = pgp(process.env.DATABASE_URL);
+app.get('/db', function (request, response) {
+  db.any('SELECT * FROM leads')
+    .then(leads =>{
+      response.render('pages/db', {results: leads} );
+    })
+    .catch(error =>{
+      console.log(error);
+    });
+/*
+  pool.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM "public.leads"', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });*/
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -15,6 +38,10 @@ app.get('/', function(request, response) {
 
 app.get('/assignments',function(req,res){
   res.render('pages/assignments')
+});
+
+app.get('/LEADer',function(req,res){
+  res.render('pages/LEADer')
 });
 
 app.get('/postalCalc', function(req,res){
